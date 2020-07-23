16eab69d779e14e4729e16089ad472a7a5389b87
unknown <testingtest0843@gmail.com>
2020-07-23 15:25:26 +0900

    add_js

---
+++ js/index_map.js
@@ -0,0 +1,6 @@
+new daum.roughmap.Lander({
+                    "timestamp" : "1594350661505",
+                    "key" : "2z6q9",
+                    "mapWidth" : "800",
+                    "mapHeight" : "438"
+                }).render();
---
+++ js/sub1_3_map.js
@@ -0,0 +1,6 @@
+                    new daum.roughmap.Lander({
+                        "timestamp" : "1594606907039",
+                        "key" : "2z8by",
+                        "mapWidth" : "1400",
+                        "mapHeight" : "500"
+                    }).render();
